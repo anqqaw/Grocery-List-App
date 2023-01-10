d7baf44998d4dc053022742d5601f58faa3947b8
Aarni Klemetti <aarni.klemetti@gmail.com>
2023-01-10 17:23:54 +0200

    Initial Commit

---
+++ GroceryList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GroceryList
+//
+//  Created by Aarni on 10.1.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Aarni on 10.1.2023.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
